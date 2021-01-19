abb5f1196d7213946e40a137b3153ec53f139f89
Artiom Kalugin <a.o.kalugin@protonmail.com>
2021-01-19 11:42:02 +0000

    Added custom hook for form.

--- src/containers/TodoContainer.jsx
+++ src/containers/TodoContainer.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {Form, List} from "../components";
+import useForm from "../hooks/useForm";
 
 const TodoContainer = () => {
+    const [input, handleInput, handleSubmit] = useForm("");
+
     return (
         <>
             <Form>
-                <Form.Base>
+                <Form.Base onSubmit={handleSubmit}>
                 <Form.Input
                     type="text"
                     placeholder="Eg. Read a book..."
+                    value={input}
+                    onChange={handleInput}
                 />
                 <Form.Button>Add</Form.Button>
                 </Form.Base>
